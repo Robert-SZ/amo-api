0d4beff3e7ef1671c6baaac076faba7156598dfa
RobertSabiryanov <robert.sabiryanov@gmail.com>
2018-04-09 18:49:14 +0500

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     amourl: "https://modulkassa.amocrm.ru",
-    amologin: "siteintegration@modulbank.ru",
-    amokey: "5e616fc564c95dbe8c2409c0bef2e2d2",
+    amologin: "",
+    amokey: "",
     mongoConn: "mongodb://mongo:27017/leadsDB",
     port: 8005
 };
