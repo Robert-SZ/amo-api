310af19d904c5a8eeec6ba274025784665e76a2c
RobertSabiryanov <robert.sabiryanov@gmail.com>
2018-05-03 19:23:20 +0500

    ok leads added

--- src/amoSocialLeadsRoutes.js
+++ src/amoSocialLeadsRoutes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
 
         let requestBody = req.body.object;
         if (!requestBody)
-            return;
+            return res.status(409).send('object body is not exists');
         let answers = requestBody.answers || [];
         let firstName = answers.filter(item => item.key === 'first_name')[0] || 'Заявка с VK';
         let email = answers.filter(item => item.key === 'email')[0];
@@ -17,7 +17,7 @@ module.exports = function (app) {
         let formName = requestBody.form_name;
 
         if (!email && !phone)
-            return;
+            return res.status(409).send('required fields are not exists');
 
 
         let data = {
@@ -34,9 +34,52 @@ module.exports = function (app) {
         };
 
         return request.post('https://api.amocore.in/modulkassa/integration/modulkassa/ouvyse51de1vrzvzchnrnnhvqtheqt09', data)
-            .then(()=>{res.status(200).send('ok')})
-            .catch(()=>{res.status(500).send('cant write to amo')});
+            .then(() => {
+                res.status(200).send('ok')
+            })
+            .catch(() => {
+                res.status(500).send('cant write to amo')
+            });
+    });
 
+    app.post('/oklead', (req, res) => {
 
+        if (!req.body.resource || req.body.resource !== 'OKLEADAD')
+            return res.status(409).send('resource is not exists');
+
+        let requestBody = req.body.diff;
+        if (!requestBody)
+            return res.status(409).send('diff body is not exists');
+        let firstName = (requestBody.user_fullname && requestBody.user_fullname['+++']) || 'Заявка с OK';
+        let email = requestBody.user_email && requestBody.user_email['+++'];
+        let phone = requestBody.user_phone && requestBody.user_phone['+++'];
+        let a_aid = requestBody.banner_id && requestBody.banner_id['+++'];
+        let formName = requestBody.form_id && requestBody.form_id['+++'];
+
+        if (!email && !phone)
+            return;
+
+        phone = phone.replace(' ', '');
+
+        let data = {
+            firstName: firstName,
+            email: email,
+            phone: phone,
+            utmCampaign: undefined,
+            utmContent: a_aid,
+            utmMedium: "cpm",
+            utmSource: "ok",
+            utmTerm: undefined,
+            formName: formName,
+            comment: JSON.stringify(requestBody)
+        };
+
+        return request.post('https://api.amocore.in/modulkassa/integration/modulkassa/ouvyse51de1vrzvzchnrnnhvqtheqt09', data)
+            .then(() => {
+                res.status(200).send('ok')
+            })
+            .catch(() => {
+                res.status(500).send('cant write to amo')
+            });
     });
 };
