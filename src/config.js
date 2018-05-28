439112d53a43cb6079761c022ecfd1015c165322
RobertSabiryanov <robert.sabiryanov@gmail.com>
2018-05-28 19:31:38 +0500

    1

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ module.exports = {
     mongoConn: "mongodb://mongo:27017/leadsDB",
     port: 8005,
     facebookVerifyToken: 'sldfjandflkajsnf',
-    facebookPageAccessToken: 'EAACO2r5k3qMBAD77ttr0aoPbulomKDfruxmznDkXfEnBvWMONRZA0N7MlWMKk5RuClgOixOSeXnOVvZAoUHvZCXNbw3kdmdISIVbtqpF3ESBBaDNHKMZB2aoO9ycyyCfXiXhuJlICcpdeU8xshul2l2nnKx15TDx7UleORrqrhk3FwGMK5RygXuVyZB2obxcZD',
+    facebookPageAccessToken: '',
     facebookApi: "https://graph.facebook.com"
 };
